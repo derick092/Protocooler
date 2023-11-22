c8e3f7f57b15a91eb4df339e1fcee77d337fc510
Dérick Igura <derick092@gmail.com>
2023-11-22 19:36:30 -0300

    Delete Protocooler.Domain directory

--- Protocooler.Domain/Protocol.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Protocooler.Domain
-{
-    public class Protocol
-    {
-        public int Company { get; set; }
-        public DateTime Date { get; set; }
-        public int Id { get; set; }
-    }
-}
